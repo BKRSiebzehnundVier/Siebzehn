a1e204ae9998d8b42a531a438063b84223f30c47
Preto95 <sebastian-giese@posteo.de>
2017-03-15 08:44:38 +0100

    added

---
+++ Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SiebzehnundVier
+{
+    abstract class Card
+    {
+        public Image CardImage { protected set; get; }
+        public Color Color { protected set; get; }
+        public Points PointS { protected set; get; }
+
+        public Card(Image cardImage, Color color)
+        {
+            this.CardImage = cardImage;
+            this.Color = color;
+        }
+
+
+
+    }
+}
---
+++ Points.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiebzehnundVier
+{
+    enum Points
+    {
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Knave = 2,
+        Queen = 3,
+        King = 4,
+        Ass = 11
+    }
+}
